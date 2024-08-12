f543e12e10e08198d8ea28c7df61573bb6316837
musmanirfan <usmanmala356@gmail.com>
2024-08-13 02:58:47 +0500

    git push

--- scripts.js
+++ scripts.js
@@ -26,21 +26,42 @@
 // }else {
 //     console.log("zero"); 
 // }
-let season = 2;
-switch (season) {
-    case 1:
-        console.log("Winter");
-        break;
-    case 2:
-        console.log("Spring");
-        break;
-    case 3:
-        console.log("summer");
-        break;
-    case 4:
-        console.log("Fall");
-        break;
-    default:
-        console.log("invalid Season");
-        break;
+// let season: number = 2
+// switch (season) {
+//     case 1:
+//         console.log("Winter");
+//         break;
+//     case 2:
+//         console.log("Spring");
+//         break;
+//     case 3:
+//         console.log("summer");
+//         break;
+//     case 4:
+//         console.log("Fall");
+//         break;
+//     default:
+//         console.log("invalid Season");
+//         break;
+// }
+// let fruits: string = "mango";
+// switch (fruits) {
+//     case "apple":
+//         console.log("This season is for Apple");
+//         break;
+//     case "banana":
+//         console.log("This season is for Banana");
+//         break;
+//     case "orange":
+//         console.log("This season is for Orange");
+//         break;
+//     default:
+//         console.log("This is not a fruit season");
+//         break;
+// }
+//loops started
+let count = 0;
+while (count <= 5) {
+    console.log(`count is ${count}`);
+    count++;
 }
--- scripts.ts
+++ scripts.ts
@@ -138,27 +138,101 @@
 
 
 
-let season : number = 2
+// let season: number = 2
+
+// switch (season) {
+//     case 1:
+//         console.log("Winter");
+//         break;
+//     case 2:
+//         console.log("Spring");
+//         break;
+//     case 3:
+//         console.log("summer");
+//         break;
+//     case 4:
+//         console.log("Fall");
+//         break;
+//     default:
+//         console.log("invalid Season");
+//         break;
+// }
 
 
-switch (season) {
-    case 1:
-        console.log("Winter");
-        break;
-        
-    case 2:
-        console.log("Spring");
-        break;
-        
-    case 3:
-        console.log("summer");
-        break;
-    case 4:
-        console.log("Fall");
-        break;
-    default:
-        console.log("invalid Season");
-        
-        break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits: string = "mango";
+
+// switch (fruits) {
+//     case "apple":
+//         console.log("This season is for Apple");
+//         break;
+//     case "banana":
+//         console.log("This season is for Banana");
+//         break;
+//     case "orange":
+//         console.log("This season is for Orange");
+//         break;
+//     default:
+//         console.log("This is not a fruit season");
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//loops started
+
+
+let count: number = 0;
+
+while (count <= 5) {
+    console.log(`count is ${count}`);
+    count++;
 }
-
