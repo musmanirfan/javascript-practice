938814b940702a1df5eabfa4e91968dd2cc74b47
musmanirfan <usmanmala356@gmail.com>
2024-08-13 02:19:47 +0500

    git push

---
+++ scripts.ts
@@ -0,0 +1,79 @@
+// let userName: string = "usman";
+
+// console.log(userName);
+
+
+
+
+
+
+
+// let square = (a: number): number => {
+//     return a * a
+// }
+
+
+// console.log(square(5)) // OUTPUT 25;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let multiply = (a: number, b:number) : number => {
+//     return a*b
+// }
+
+
+// console.log(multiply(5,10));// output 50
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let sayHello = (name: string) : string => {
+//     return `Hello ${name} !`
+// }
+
+// console.log(sayHello("usman"));// Hello usman !
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let even = (num: number) : boolean =>{
+    return num & 1 ? false : true
+}
+
+console.log(even(5));
+
+
+
